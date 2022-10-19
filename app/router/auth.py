4c5c6788f309f2b7247f72c8c2458e6b971ec7d1
varvara <varvarashcherbyna7@gmail.com>
2022-10-19 15:35:48 +0300

    auth

--- app/router/auth.py
+++ app/router/auth.py
@@ -5,6 +5,8 @@ from app import schema
 from app.database import get_db
 from app.model import User
 from app.oauth2 import create_access_token
+from app.logger import log
+
 
 auth_router = APIRouter(tags=["Authentication"])
 
@@ -40,16 +42,25 @@ def login(
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@auth_router.post("/google_login")
+@auth_router.post("/google_login", response_model=schema.Token)
 def google_login(user_data: schema.UserGoogleLogin, db: Session = Depends(get_db),):
-    user_data
 
-    user: User = User
+    # user: User = db.query(User).filter_by(email=user_data.email)
+    user = db.query(User).filter(User.email == user_data.email).first()
 
-    # if not user:
-    #     raise HTTPException(status_code=403, detail="Invalid credentials")
+    if not user:
+        log(log.INFO, "Client [%s] must create", user_data.email)
+        user = User(
+            email=user_data.email,
+            google_openid_key=user_data.google_openid_key,
+            picture=user_data.picture,
+            username=user_data.username,
+            verified=True,
+        )
+        db.add(user)
+        db.commit()
+        db.refresh(user)
 
-    # access_token = create_access_token(data={"user_id": user.id})
+    access_token = create_access_token(data={"user_id": user.id})
 
-    # return {"access_token": access_token, "token_type": "bearer"}
-    # return 'ok'
+    return {"access_token": access_token, "token_type": "bearer"}
